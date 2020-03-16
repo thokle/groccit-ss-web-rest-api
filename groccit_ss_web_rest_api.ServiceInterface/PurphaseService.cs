016caf0ba48e73861c2ef0c52f5d9134de803a12
Thomas Kleist <thomas@kleist-it.com>
2020-03-16 21:18:05 +0100

    fixed purphaelist

--- groccit_ss_web_rest_api.ServiceInterface/PurphaseService.cs
+++ groccit_ss_web_rest_api.ServiceInterface/PurphaseService.cs
@@ -17,7 +17,7 @@ namespace groccit_ss_web_rest_api.ServiceInterface
         
         public object Any(Purphase purphase)
         {
-            var testpurphaoe = (data_soruce_mil.models.TestPurchaseIntention)purphase.testPurchase;eld 
+            var testpurphaoe = (data_soruce_mil.models.TestPurchaseIntention)purphase.testPurchase;
 
           var res =   testPurhase.CreateTestPurchaseIntention(testpurphaoe);
            
